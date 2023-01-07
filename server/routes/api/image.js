2d376f67117be4a2287f5b9c682a8093ccdc1145
Mateusz Derela <78499513+DevMateusz@users.noreply.github.com>
2023-01-07 15:37:41 +0100

    readme

--- server/routes/api/image.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const imageController = require('../../controllers/imageController');
-
-router.get('/:id', imageController.getImage);
-
-module.exports = router;
